7e445863b18509157c9c76476fe7ec02126aeb7d
Tero Taipalus <tero.taipalus@tuni.fi>
2020-04-21 19:56:03 +0300

    Javadoc fixes #4

--- app/src/main/java/com/tuni/isthisenough/HourObject.java
+++ app/src/main/java/com/tuni/isthisenough/HourObject.java
@@ -30,40 +30,93 @@ public class HourObject {
         this.oDate = date;
     }
 
+    /**
+     * Get method for job title.
+     * @return returns the value of job title.
+     */
     public String getJobTitle() {
         return jobTitle;
     }
+
+    /**
+     * Set method for job title.
+     * @param jobTitle input value to be set.
+     */
 
     public void setJobTitle(String jobTitle) {
         this.jobTitle = jobTitle;
     }
 
+    /**
+     * Get method for job hours.
+     * @return returns the value of hours.
+     */
+
     public int getoHours() {
         return oHours;
     }
+
+    /**
+     * Set method for job hours.
+     * @param oHours input value to be set.
+     */
 
     public void setoHours(int oHours) {
         this.oHours = oHours;
     }
 
+    /**
+     * Get method for job minutes.
+     * @return returns the value of minutes.
+     */
+
     public int getoMinutes() {
         return oMinutes;
     }
+
+    /**
+     * Set method for job hours.
+     * @param oMinutes input value to be set.
+     */
 
     public void setoMinutes(int oMinutes) {
         this.oMinutes = oMinutes;
     }
 
+    /**
+     * Get method for job description.
+     * @return returns job description.
+     */
+
     public String getJodDescription() {
         return jodDescription;
     }
+
+    /**
+     * Set method for job description.
+     * @param jodDescription input value to be set.
+     */
 
     public void setJodDescription(String jodDescription) {
         this.jodDescription = jodDescription;
     }
 
+    /**
+     * Get method for date.
+     * @return the date
+     */
+
     public String getDate() {
         return oDate;
+    }
+
+    /**
+     * Set method for date
+     * @param date input value to be set.
+     */
+
+    public void setDate(String date) {
+        this.oDate = oDate;
     }
 
     /**
